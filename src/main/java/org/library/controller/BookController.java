6a3a7e3583ce1c469064fe0b4be4f7ed7149865b
Mo7ammedFawzy <devfawzey@gmail.com>
2026-05-20 19:19:29 +0300

    modify BookController

--- src/main/java/org/library/controller/BookController.java
+++ src/main/java/org/library/controller/BookController.java
@@ -1,6 +1,25 @@
 package org.library.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.library.core.ApiResponse;
+import org.library.dto.BookResponse;
+import org.library.service.impl.BookServiceImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
+@RequiredArgsConstructor
 public class BookController
 {
+	private final BookServiceImpl bookService;
 
+	@GetMapping
+	public ResponseEntity<ApiResponse<List<BookResponse>>> getAllBooks()
+	{
+		List<BookResponse> allBooks = this.bookService.getAllBooks();
+		return ResponseEntity.ok().body(ApiResponse.<List<BookResponse>>create().data(allBooks).build());
+	}
 }
