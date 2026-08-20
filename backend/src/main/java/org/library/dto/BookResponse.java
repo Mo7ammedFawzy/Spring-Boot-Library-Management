b45514bf7a39ea02e9d2c34ffab175bb1d7e253e
Mo7ammedFawzy <devfawzey@gmail.com>
2026-08-20 20:53:50 +0300

    add spaces

--- backend/src/main/java/org/library/dto/BookResponse.java
+++ backend/src/main/java/org/library/dto/BookResponse.java
@@ -2,6 +2,10 @@ package org.library.dto;
 
 import java.util.List;
 
+
+
+
+
 public record BookResponse(Long id, String title, String description, Long availableCopies, CategoryResponse category, List<AuthorResponse> authors)
 {
 }
